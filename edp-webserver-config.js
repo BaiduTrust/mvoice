756ee9c2b699d88b0c5d6228c321ea4ed3cb2d45
cgzero <cgzero@cgzero.com>
2016-06-30 16:17:28 +0800

    update edp-webserver-config

--- edp-webserver-config.js
+++ edp-webserver-config.js
@@ -20,17 +20,18 @@ exports.getLocations = function () {
                 livereload()
             ]
         },
+        // 因为发送微信请求的时候，会以当前url作为参数，所以php的处理要先于html的处理
+        {
+            location: /\.php($|\?)/,
+            handler: [
+                php()
+            ]
+        },
         {
             location: /\.html($|\?)/,
             handler: [
                 file(),
                 livereload()
-            ]
-        },
-        {
-            location: /\.php($|\?)/,
-            handler: [
-                php()
             ]
         },
         {
